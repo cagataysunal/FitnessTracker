ef6a9f2e8422a733385eadcbfcae3e7bcc9c1423
Çağatay Sunal <cagataysunal9199@gmail.com>
2026-07-21 01:08:48 +0300

    Add kotlinx serialization

--- FitnessTracker.APP/app/build.gradle.kts
+++ FitnessTracker.APP/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -47,6 +48,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.retrofit)
+    implementation(libs.kotlin.serialization)
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
