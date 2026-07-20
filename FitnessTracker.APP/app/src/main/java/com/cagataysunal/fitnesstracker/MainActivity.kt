e687508a07e1e9a25a5342c16f5b87ab487978e5
Çağatay Sunal <cagataysunal9199@gmail.com>
2026-07-21 00:23:35 +0300

    Remove unused imports

--- FitnessTracker.APP/app/src/main/java/com/cagataysunal/fitnesstracker/MainActivity.kt
+++ FitnessTracker.APP/app/src/main/java/com/cagataysunal/fitnesstracker/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.cagataysunal.fitnesstracker
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,11 +14,9 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import com.cagataysunal.fitnesstracker.ui.theme.FitnessTrackerTheme
 
